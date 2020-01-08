8946569473ca3719322da55a351c3b6f09cf935b
Jordy van Ekelen <jordyvanekelen@gmail.com>
2020-01-08 12:23:13 +0100

    Removed html2canvas print.

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -77,24 +77,4 @@ public printScreen()
     location.reload();
 }
 
-public captureScreen()  
-  {  
-    var data = document.getElementById('contentToConvert');  //Id of the table
-    html2canvas(data, {
-      useCORS : true
-    }).then(canvas => {  
-      // Few necessary setting options  
-
-      let imgWidth = 208;   
-      let pageHeight = 295;    
-      let imgHeight = canvas.height * imgWidth / canvas.width;  
-      let heightLeft = imgHeight;  
-
-      const contentDataURL = canvas.toDataURL('activities/5df0f2a953b1b20017a7b821')  
-      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
-      let position = -40;  
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-      pdf.save(this.activity[0]['activity'] + '.pdf'); // Generated PDF   
-    });  
-  }  
 }
