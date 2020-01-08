ab1af1b0c9677886ec3ee075665e2dfdefba55d3
Jordy van Ekelen <jordyvanekelen@gmail.com>
2020-01-08 11:38:56 +0100

    Updated cart page. Implemented PDF functionality.

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -4,6 +4,7 @@ import * as jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';  
 import { ActivatedRoute } from '@angular/router';
 import { Activity } from '../models/Activity';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-activities',
@@ -14,7 +15,8 @@ import { Activity } from '../models/Activity';
 export class ActivitiesComponent implements OnInit {
   
   activity : Activity;
- 
+  printing = false;
+
   constructor(
     private apiService: ApiServiceService,
     private activatedRoute: ActivatedRoute 
@@ -40,11 +42,39 @@ ngOnInit() {
 
 public printScreen()
 {
-    var originalContents = document.body.innerHTML;
-    var printContents = document.getElementById('inner').innerHTML;
+  var originalContents = document.body.innerHTML;
+
+    // this.printing = true;
+    var banner = document.getElementById('banner');
+    var inner = document.getElementById('test');
+    var customimg = document.getElementById('customimgs');
+    var main_image = document.getElementById('main_image');
+    var main_content = document.getElementById('main_content');
+    var main_title = document.getElementById('main_title');
+
+    main_image.style.position = "absolute";
+    main_image.style.marginTop = "270px";
+    main_content.style.marginTop = "360px";
+    
+    main_title.style.marginTop = "210px"
+    main_title.style.marginLeft = "450px"
+    main_title.style.position = "absolute";
+
+    document.body.style.width = "100%";
+    document.documentElement.style.width = "100%"
+    
+    banner.style.position = "absolute";
+    banner.style.top = '0px';
+    banner.style.left= '0px';
+
+    inner.style.marginTop = "200px";
+    customimg.style.marginTop = "70px";
+
+    var printContents = document.getElementById('outer').innerHTML;
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents;
+    location.reload();
 }
 
 public captureScreen()  
