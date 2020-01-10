9dc65cdbbdd9be81b22110af3b298c3566109824
Jordy van Ekelen <jordyvanekelen@gmail.com>
2020-01-10 21:03:23 +0100

    Updated test.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,17 +7,18 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
+  
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).not.toEqual('siza app is running!');
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
-});
